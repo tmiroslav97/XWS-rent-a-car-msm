c2bf1a28372024fe3d8c09fd067a994ca3a717c9
Magdalena <magdalena.lakic@gmail.com>
2020-04-25 15:05:47 +0200

    Finished hibernate mapping for car.

--- MonolitApp/agent-app/src/main/java/agent/app/common/db/DbColumnConstants.java
+++ MonolitApp/agent-app/src/main/java/agent/app/common/db/DbColumnConstants.java
@@ -7,4 +7,16 @@ public class DbColumnConstants {
     public static final String LASTNAME = "lastName";
     public static final String NAME = "name";
     public static final String LASTPASSWORDRESETDATE = "lastPasswordResetDate";
+    public static final String YEAR = "year";
+    public static final String CARMANUFACTURER = "carManufacturer";
+    public static final String CARMODEL = "carModel";
+    public static final String GEARBOXTYPE = "gearboxType";
+    public static final String FUELTYPE = "fuelType";
+    public static final String CARTYPE = "carType";
+    public static final String MILEAGE = "mileage";
+    public static final String CHILDRENSEATNUM = "childrenSeatNum";
+    public static final String CDW = "cdw";
+    public static final String ANDROIDFLAG = "androidFlag";
+    public static final String TOKEN = "token";
+
 }
--- MonolitApp/agent-app/src/main/java/agent/app/common/db/DbTableConstants.java
+++ MonolitApp/agent-app/src/main/java/agent/app/common/db/DbTableConstants.java
@@ -4,4 +4,5 @@ public class DbTableConstants {
     public static final String USER = "user";
     public static final String AUTHORITY = "authority";
     public static final String USERAUTHORITY = "userAuthority";
+    public static final String CAR = "car";
 }
