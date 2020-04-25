4de31321ffd331e847548b5665fc9da08412e433
Magdalena <magdalena.lakic@gmail.com>
2020-04-25 20:46:48 +0200

    Created DTO for cat

---
+++ MonolitApp/agent-app/src/main/java/agent/app/dto/AdPageDTO.java
@@ -0,0 +1,19 @@
+package agent.app.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class AdPageDTO {
+
+    private String name;
+    private String coverPhoto;
+    private String location;
+    private Float price;
+
+
+}
