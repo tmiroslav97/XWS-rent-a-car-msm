af4eafd78f1a9458623407cf978d6e22869837fa
Magdalena <magdalena.lakic@gmail.com>
2020-04-26 13:56:22 +0200

    Implemented method for pagination.

--- MonolitApp/agent-app/src/main/java/agent/app/controller/AdController.java
+++ MonolitApp/agent-app/src/main/java/agent/app/controller/AdController.java
@@ -1,17 +1,18 @@
 package agent.app.controller;
 
 import agent.app.dto.AdCreateDTO;
+import agent.app.dto.AdPageDTO;
 import agent.app.service.intf.AdService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/ad")
+@RequestMapping(value = "/ad",  produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdController {
 
     private final AdService adService;
@@ -20,9 +21,15 @@ public class AdController {
         this.adService = adService;
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> createAd(@RequestBody AdCreateDTO adCreateDTO){
         return new ResponseEntity<>(adService.createAd(adCreateDTO), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "{?page,size,sort}", method = RequestMethod.GET)
+    public ResponseEntity<?> findAllPageAd(@PathVariable("page") Integer page,@PathVariable("size") Integer size,
+                                         @PathVariable("sort") String sort) {
+        return new ResponseEntity<>(adService.findAllPageAd(page, size, sort), HttpStatus.OK);
     }
 
 }
--- MonolitApp/agent-app/src/main/java/agent/app/repository/AdRepository.java
+++ MonolitApp/agent-app/src/main/java/agent/app/repository/AdRepository.java
@@ -1,10 +1,17 @@
 package agent.app.repository;
 
 import agent.app.model.Ad;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdRepository extends JpaRepository<Ad,Long> {
+
+    Page<Ad> findAllByDeleted(Boolean deleted, Pageable pageable);
 
 }
