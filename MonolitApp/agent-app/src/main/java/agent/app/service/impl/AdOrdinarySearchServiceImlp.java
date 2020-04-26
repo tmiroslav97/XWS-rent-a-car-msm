87de136c79e4e4a1279c4f39c4ab4edaf9ce7699
Magdalena <magdalena.lakic@gmail.com>
2020-04-26 15:41:50 +0200

    Created services and repository for ordinary search.

---
+++ MonolitApp/agent-app/src/main/java/agent/app/service/impl/AdOrdinarySearchServiceImlp.java
@@ -0,0 +1,8 @@
+package agent.app.service.impl;
+
+import agent.app.service.intf.AdOrdinarySearchService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdOrdinarySearchServiceImlp implements AdOrdinarySearchService {
+}
