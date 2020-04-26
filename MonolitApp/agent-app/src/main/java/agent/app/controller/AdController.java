9574f7a1e21443a607b1238f5c7d63469ed9054b
Svetlana <antesevicceca@gmail.com>
2020-04-26 16:05:07 +0200

    Add authorization on method createdAd

--- MonolitApp/agent-app/src/main/java/agent/app/controller/AdController.java
+++ MonolitApp/agent-app/src/main/java/agent/app/controller/AdController.java
@@ -5,6 +5,7 @@ import agent.app.service.intf.AdService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ public class AdController {
         this.adService = adService;
     }
 
+    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT')")
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createAd(@RequestBody AdCreateDTO adCreateDTO){
         return new ResponseEntity<>(adService.createAd(adCreateDTO), HttpStatus.CREATED);
