0dc4d613d2f68b2a3e9a0827f6da67cd19517943
Miroslav <tomic.miroslav97@gmail.com>
2020-05-05 16:45:20 +0200

    Adding data to script

--- MonolitApp/agent-app/src/main/resources/data.sql
+++ MonolitApp/agent-app/src/main/resources/data.sql
@@ -5,8 +5,14 @@ VALUES ('ROLE_AGENT');
 INSERT INTO authority (name)
 VALUES ('ROLE_USER');
 
-INSERT INTO user(email, first_name, last_name, password)
-VALUES ('tomic@tomic.com', 'Tomke', 'Faker', '$2a$10$VSlWn0nzWDB2Jxv7cx.sf.NakwjllWrSjdkWi66g2dMM.OdBGThlS');
+INSERT INTO user(id, email, first_name, last_name, password)
+VALUES (1, 'tomic@tomic.com', 'Tomke', 'Dazdarac', '$2a$10$VSlWn0nzWDB2Jxv7cx.sf.NakwjllWrSjdkWi66g2dMM.OdBGThlS');
 
 INSERT INTO user_authority(user_id, authority_id)
 VALUES (1,2);
+
+INSERT INTO user(id, email, first_name, last_name, password)
+VALUES (2, 'tomic.miroslav97@gmail.com', 'Miroslav', 'Tomic', '$2a$10$VSlWn0nzWDB2Jxv7cx.sf.NakwjllWrSjdkWi66g2dMM.OdBGThlS');
+
+INSERT INTO user_authority(user_id, authority_id)
+VALUES (2,1);
