61622874f4246ea1122df108160cc0fcc3b90af5
Magdalena <magdalena.lakic@gmail.com>
2020-05-20 21:53:38 +0200

    Uploaded pom file in price list and discount service

--- Services/price-list-and-discount-service/src/main/java/services/app/pricelistanddiscountservice/PriceListAndDiscountServiceApplication.java
+++ Services/price-list-and-discount-service/src/main/java/services/app/pricelistanddiscountservice/PriceListAndDiscountServiceApplication.java
@@ -2,8 +2,10 @@ package services.app.pricelistanddiscountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PriceListAndDiscountServiceApplication {
 
 	public static void main(String[] args) {
