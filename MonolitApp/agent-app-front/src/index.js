2f0015d38034c31445e7a23f312b29b99aad0cb0
Svetlana <antesevicceca@gmail.com>
2020-05-26 19:50:40 +0200

    Edited index.js file from agent-app-front service.

--- MonolitApp/agent-app-front/src/index.js
+++ MonolitApp/agent-app-front/src/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, HashRouter, BrowserRouter } from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createBrowserHistory, createHashHistory } from 'history';
+import {ncreateHashHistory } from 'history';
 import store from './store';
 import AppRouter from './AppRouter';
 import NavBar from './containers/NavBar';
 import InformationToastsContainer from './containers/Common/InformationToastsContainer';
 import { ToastProvider } from 'react-toast-notifications';
 
-export const history = createBrowserHistory();
-// export const history = createHashHistory();
+export const history = createHashHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <HashRouter history={history}>
       <NavBar />
       <AppRouter />
       <ToastProvider>
         <InformationToastsContainer />
       </ToastProvider>
-    </Router>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
