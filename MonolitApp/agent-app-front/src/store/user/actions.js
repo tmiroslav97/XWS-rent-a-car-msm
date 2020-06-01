45e87bdc7d2b3ce7a6947d0a66e2da49cbd02af8
Miroslav <tomic.miroslav97@gmail.com>
2020-06-01 21:25:47 +0200

    Adding constants and actions on frontend

--- MonolitApp/agent-app-front/src/store/user/actions.js
+++ MonolitApp/agent-app-front/src/store/user/actions.js
@@ -2,9 +2,35 @@ import {
     LOGIN,
     PUT_TOKEN,
     REGISTER_USER,
-    SIGN_OUT
+    SIGN_OUT,
+    FETCH_END_USERS_PAGINATED,
+    BLOCK_OR_UNBLOCK,
+    OBLIGATE_OR_UNOBLIGATE,
+    LOG_DEL_OR_REVERT
 } from './constants';
 
+//end users
+export const fetchEndUsersPaginated = payload => ({
+    type: FETCH_END_USERS_PAGINATED,
+    payload
+});
+
+export const blockOrUnblock = payload => ({
+    type: BLOCK_OR_UNBLOCK,
+    payload
+});
+
+export const obligateOrUnobligate = payload => ({
+    type: OBLIGATE_OR_UNOBLIGATE,
+    payload
+});
+
+export const logDelOrRevert = payload => ({
+    type: LOG_DEL_OR_REVERT,
+    payload
+});
+
+//users
 export const signOut = payload => ({
     type: SIGN_OUT,
     payload
--- MonolitApp/agent-app-front/src/store/user/constants.js
+++ MonolitApp/agent-app-front/src/store/user/constants.js
@@ -3,3 +3,9 @@ export const PUT_TOKEN = 'put_token';
 export const FETCH_TOKEN = 'fetch_token';
 export const REGISTER_USER = 'register_user';
 export const SIGN_OUT = 'sign_out';
+
+//enduser
+export const FETCH_END_USERS_PAGINATED = 'fetch_end_users_paginated'; 
+export const BLOCK_OR_UNBLOCK = 'block_or_unblock';
+export const OBLIGATE_OR_UNOBLIGATE = 'obligate_or_unobligate';
+export const LOG_DEL_OR_REVERT = 'log_del_or_revert';
