421e8e330552dc02d1305a46b4d5264db46149dd
Svetlana <antesevicceca@gmail.com>
2020-06-01 22:46:28 +0200

    Made publisher user service interface.

---
+++ MonolitApp/agent-app/src/main/java/agent/app/service/intf/PublisherUserService.java
@@ -0,0 +1,18 @@
+package agent.app.service.intf;
+
+import agent.app.dto.car.CarCalendarTermCreateDTO;
+import agent.app.model.CarCalendarTerm;
+import agent.app.model.PublisherUser;
+
+import java.util.List;
+
+
+public interface PublisherUserService {
+    PublisherUser findById(Long id);
+    List<PublisherUser> findAll();
+    PublisherUser save(PublisherUser publisherUser);
+    void delete(PublisherUser publisherUser);
+    Integer deleteById(Long id);
+    PublisherUser createPublisherUser(PublisherUser publisherUser);
+    PublisherUser editPublisherUser(PublisherUser publisherUser);
+}
