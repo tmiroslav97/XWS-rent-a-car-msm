43267a30ae2e7330c3661d85f888f428d9db9289
Svetlana <antesevicceca@gmail.com>
2020-06-02 14:29:59 +0200

    Added basic method to Image service.

--- MonolitApp/agent-app/src/main/java/agent/app/service/impl/ImageServiceImpl.java
+++ MonolitApp/agent-app/src/main/java/agent/app/service/impl/ImageServiceImpl.java
@@ -1,9 +1,46 @@
 package agent.app.service.impl;
 
+import agent.app.model.Image;
 import agent.app.service.intf.ImageService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ImageServiceImpl implements ImageService {
-    
+
+    @Override
+    public Image finById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Image> findAll() {
+        return null;
+    }
+
+    @Override
+    public Image save(Image image) {
+        return null;
+    }
+
+    @Override
+    public void delete(Image image) {
+
+    }
+
+    @Override
+    public Image createImage(Image image) {
+        return null;
+    }
+
+    @Override
+    public Image editImage(Image image) {
+        return null;
+    }
+
+    @Override
+    public Integer deleteById(Long id) {
+        return null;
+    }
 }
