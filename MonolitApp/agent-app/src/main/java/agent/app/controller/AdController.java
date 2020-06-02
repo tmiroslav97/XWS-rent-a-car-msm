22a11e1b113f276e9c122ec012424fec70d9207a
Svetlana <antesevicceca@gmail.com>
2020-06-02 02:02:17 +0200

    Made method for upload image in ad controller.

--- MonolitApp/agent-app/src/main/java/agent/app/controller/AdController.java
+++ MonolitApp/agent-app/src/main/java/agent/app/controller/AdController.java
@@ -1,6 +1,7 @@
 package agent.app.controller;
 
 import agent.app.dto.ad.AdCreateDTO;
+import agent.app.model.CarManufacturer;
 import agent.app.service.intf.AdService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,40 +27,49 @@ public class AdController {
 
     ObjectMapper objectMapper = new ObjectMapper();
 
-
     @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT')")
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> createAd(@RequestParam(value="coverPhoto", required = true) MultipartFile coverPhoto, @RequestParam(value="data", required = true)  String data) throws IOException{
+    public ResponseEntity<?> uploadImage(@RequestParam(value = "coverPhoto", required = true) MultipartFile coverPhoto) throws IOException {
         System.out.println("-----------------------UPLOAD FILE---------------------");
-        System.out.println("slika : " + coverPhoto.getOriginalFilename());
+
         File file = new File("photos");
-        String direktorijum = file.getAbsolutePath() + "\\" + coverPhoto.getOriginalFilename();
-        System.out.println(direktorijum);
-        File convertFile = new File(direktorijum.toString());
+        String uploadDirectory = file.getAbsolutePath() + "\\" + coverPhoto.getOriginalFilename();
+
+        System.out.println(uploadDirectory);
+        System.out.println("slika : " + coverPhoto.getOriginalFilename());
+
+        File convertFile = new File(uploadDirectory.toString());
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
         fout.write(coverPhoto.getBytes());
         fout.close();
+        //TODO 1: POZVATI METODE IMAGE SERVISA ZA UPLOAD SLIKE
+        return new ResponseEntity<>("Slika uspesno dodata.", HttpStatus.CREATED);
+    }
 
-        System.out.println(data.toString());
-        AdCreateDTO adCreateDTO = objectMapper.readValue(data, AdCreateDTO.class);
-        adCreateDTO.setCoverPhoto(coverPhoto.getOriginalFilename());
+    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT')")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createAd(@RequestBody AdCreateDTO adCreateDTO) {
+        System.out.println(adCreateDTO);
+//        AdCreateDTO adCreateDTO = objectMapper.readValue(data, AdCreateDTO.class);
+//        adCreateDTO.setCoverPhoto(coverPhoto.getOriginalFilename());
         Integer flag = adService.createAd(adCreateDTO);
-        if(flag == 1){
+        if (flag == 1) {
             return new ResponseEntity<>("Oglas uspesno kreiran.", HttpStatus.CREATED);
-        }else{
+        } else {
             return new ResponseEntity<>("Desila se greska.", HttpStatus.BAD_REQUEST);
         }
 
     }
 
-//    @RequestMapping(value = "{?page,size,sort}", method = RequestMethod.GET)
+    //    @RequestMapping(value = "{?page,size,sort}", method = RequestMethod.GET)
 //    public ResponseEntity<?> findAllPageAd(@PathVariable("page") Integer page,@PathVariable("size") Integer size,
 //                                         @PathVariable("sort") String sort) {
 //        return new ResponseEntity<>(adService.findAllPageAd(page, size, sort), HttpStatus.OK);
 //    }
-@PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT') or hasAuthority('ROLE_ADMIN')")
-@RequestMapping(method = RequestMethod.GET)
+
+    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT') or hasAuthority('ROLE_ADMIN')")
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> findAllPageAd(@RequestParam(value = "nextPage", required = false) Integer nextPage, @RequestParam(value = "size", required = false) Integer size) {
 
         if (nextPage != null) {
