b84a3e69e2a8e7c4ec20fac35b5e642a80ad8616
Svetlana <antesevicceca@gmail.com>
2020-06-02 14:52:13 +0200

    Implemented methods in image service.

--- MonolitApp/agent-app/src/main/java/agent/app/service/impl/ImageServiceImpl.java
+++ MonolitApp/agent-app/src/main/java/agent/app/service/impl/ImageServiceImpl.java
@@ -1,7 +1,12 @@
 package agent.app.service.impl;
 
+import agent.app.exception.ExistsException;
+import agent.app.exception.NotFoundException;
 import agent.app.model.Image;
+import agent.app.repository.CarRepository;
+import agent.app.repository.ImageRepository;
 import agent.app.service.intf.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,38 +14,47 @@ import java.util.List;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     @Override
     public Image finById(Long id) {
-        return null;
+        return imageRepository.findById(id).orElseThrow(()-> new NotFoundException("Slika ne postoji"));
     }
 
     @Override
     public List<Image> findAll() {
-        return null;
+        return imageRepository.findAll();
     }
 
     @Override
     public Image save(Image image) {
-        return null;
+        if(imageRepository.existsById(image.getId())){
+            throw new ExistsException(String.format("Slika vec postoji."));
+        }
+        return imageRepository.save(image);
     }
 
     @Override
     public void delete(Image image) {
-
+        imageRepository.delete(image);
     }
 
     @Override
     public Image createImage(Image image) {
-        return null;
+        return this.save(image);
     }
 
     @Override
     public Image editImage(Image image) {
-        return null;
+        this.finById(image.getId());
+        return this.save(image);
     }
 
     @Override
     public Integer deleteById(Long id) {
-        return null;
+        Image image = this.finById(id);
+        this.delete(image);
+        return 1;
     }
 }
