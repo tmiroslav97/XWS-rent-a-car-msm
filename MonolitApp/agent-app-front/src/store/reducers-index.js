ad048908306e0f6e187b2a48663263fae1f7ce02
Miroslav <tomic.miroslav97@gmail.com>
2020-06-04 00:37:05 +0200

    Adding store for requests and cart

--- MonolitApp/agent-app-front/src/store/reducers-index.js
+++ MonolitApp/agent-app-front/src/store/reducers-index.js
@@ -4,12 +4,14 @@ import userReducer from './user/reducer';
 import commonReducer from './common/reducer';
 import adReducer from './ad/reducer';
 import codebookReducer from './codebook/reducer';
+import requestCartReducer from './request-cart/reducer';
 
 const rootReducer = combineReducers({
     userReducer,
     commonReducer,
     adReducer,
-    codebookReducer
+    codebookReducer,
+    requestCartReducer
 });
 
 
--- MonolitApp/agent-app-front/src/store/sagas-index.js
+++ MonolitApp/agent-app-front/src/store/sagas-index.js
@@ -4,6 +4,7 @@ import flatten from 'lodash/flatten';
 import * as userSaga from './user/saga';
 import * as adSaga from './ad/saga';
 import * as codebookSaga from './codebook/saga';
+import * as requestCartSaga from './request-cart/saga';
 
 
 import {
@@ -15,7 +16,8 @@ export default function* rootSaga() {
     [
       userSaga,
       adSaga,
-      codebookSaga
+      codebookSaga,
+      requestCartSaga
     ].map(saga => Object.keys(saga).map(sagaFunctionName => saga[sagaFunctionName]))
   );
 
