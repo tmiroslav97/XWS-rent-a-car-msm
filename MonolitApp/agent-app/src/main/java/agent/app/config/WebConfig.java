c911fc4bf54bf818854ce051d5afe9acb0152c71
Miroslav <tomic.miroslav97@gmail.com>
2020-06-05 18:27:22 +0200

    Refactoring gateway and security configuration

--- MonolitApp/agent-app/src/main/java/agent/app/config/WebConfig.java
+++ MonolitApp/agent-app/src/main/java/agent/app/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,7 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 
--- Services/codebook-service/src/main/java/services/app/codebookservice/controller/CarModelController.java
+++ Services/codebook-service/src/main/java/services/app/codebookservice/controller/CarModelController.java
@@ -19,7 +19,7 @@ public class CarModelController {
         this.carModelService = carModelService;
     }
 
-    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT') or hasAuthority('ROLE_ADMIN')")
+    //@PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_AGENT') or hasAuthority('ROLE_ADMIN')")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> getAll(@RequestParam(value = "nextPage", required = false) Integer nextPage, @RequestParam(value = "size", required = false) Integer size) {
         if (nextPage != null) {
