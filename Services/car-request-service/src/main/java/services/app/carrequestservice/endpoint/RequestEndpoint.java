c90c34088eea2edd2304146fbd6af4e3112a48e2
Miroslav <tomic.miroslav97@gmail.com>
2020-06-20 16:13:00 +0200

    Implemented soap side for fetching requests by publisher user

--- Services/car-request-service/src/main/java/services/app/carrequestservice/endpoint/RequestEndpoint.java
+++ Services/car-request-service/src/main/java/services/app/carrequestservice/endpoint/RequestEndpoint.java
@@ -1,5 +1,6 @@
 package services.app.carrequestservice.endpoint;
 
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
@@ -7,6 +8,7 @@ import services.app.carrequestservice.model.GetAllReqeustsByPublisherIdRequest;
 import services.app.carrequestservice.model.GetAllRequestsByPublisherIdResponse;
 import services.app.carrequestservice.service.intf.RequestService;
 
+@Endpoint
 public class RequestEndpoint {
     private static final String NAMESPACE_URI = "http://www.app.services/carrequestservice/model";
 
@@ -16,9 +18,9 @@ public class RequestEndpoint {
         this.requestService = requestService;
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllReqeustsByPublisherIdRequest")
     @ResponsePayload
-    public GetAllRequestsByPublisherIdResponse getCountry(@RequestPayload GetAllReqeustsByPublisherIdRequest request) {
+    public GetAllRequestsByPublisherIdResponse getAllRequestsByPublisherIdResponse(@RequestPayload GetAllReqeustsByPublisherIdRequest request) {
         GetAllRequestsByPublisherIdResponse response = new GetAllRequestsByPublisherIdResponse();
         response.getRequests().addAll(requestService.findAllByPublisherUserId(request.getPublisherUser()));
         return response;
