a69209d012e0f1e9ebc4acee41ffe4ba96fc786e
Miroslav <tomic.miroslav97@gmail.com>
2020-06-20 14:28:31 +0200

    Adding initial endpoint

--- Services/car-request-service/src/main/java/services/app/carrequestservice/endpoint/RequestEndpoint.java
+++ Services/car-request-service/src/main/java/services/app/carrequestservice/endpoint/RequestEndpoint.java
@@ -1,9 +1,14 @@
 package services.app.carrequestservice.endpoint;
 
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import services.app.carrequestservice.model.GetAllReqeustsByPublisherIdRequest;
+import services.app.carrequestservice.model.GetAllRequestsByPublisherIdResponse;
 import services.app.carrequestservice.service.intf.RequestService;
 
 public class RequestEndpoint {
-    private static final String NAMESPACE_URI = "http://www.app.services/carrequestservice/gen";
+    private static final String NAMESPACE_URI = "http://www.app.services/carrequestservice/model";
 
     private final RequestService requestService;
 
@@ -11,4 +16,11 @@ public class RequestEndpoint {
         this.requestService = requestService;
     }
 
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetAllRequestsByPublisherIdResponse getCountry(@RequestPayload GetAllReqeustsByPublisherIdRequest request) {
+        GetAllRequestsByPublisherIdResponse response = new GetAllRequestsByPublisherIdResponse();
+        response.getRequests().addAll(requestService.findAllByPublisherUserId(request.getPublisherUser()));
+        return response;
+    }
 }
